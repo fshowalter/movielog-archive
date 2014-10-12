c37073c246331279e9b601f14e92a75dbba1a242
Frank Showalter <fshowalter@gmail.com>
2014-10-11 20:18:33 -0400

    beef up review descriptions

--- movielog/helpers/description_for_review.rb
+++ movielog/helpers/description_for_review.rb
@@ -3,12 +3,17 @@ module Movielog
   # Responsible for providing template helper methods.
   #
   module Helpers
-    def description_for_review(review:, aka_titles:)
-      description = "A review of #{review.display_title}"
+    def description_for_review(review:, aka_titles:, headline_cast:)
+      description = "A review of the movie #{review.display_title}"
 
-      return "#{description}." unless aka_titles && aka_titles.any?
+      if aka_titles && aka_titles.any?
+        aka_titles = aka_titles.select { |t| t != review.display_title }
+        description = "#{description} (also known as #{aka_titles.to_sentence})"
+      end
 
-      "#{description}, also known as #{aka_titles.to_sentence}."
+      headline_cast = headline_cast.map { |p| "#{p.first_name} #{p.last_name}" }
+
+      "#{description} starring #{headline_cast.to_sentence}."
     end
   end
 end
