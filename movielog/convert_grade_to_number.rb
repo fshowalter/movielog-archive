8c3db87c32a01e12aef4c99ad0b546a9b956203f
Frank Showalter <fshowalter@gmail.com>
2014-12-06 21:00:30 -0500

    add viewings fix rating numbers

--- movielog/convert_grade_to_number.rb
+++ movielog/convert_grade_to_number.rb
@@ -5,18 +5,18 @@ module Movielog
   class ConvertGradeToNumber
     NUMBER_FOR_LETTER_GRADE = {
       'A+' => 15,
-      'A' => 14,
-      'A-' => 13,
-      'B+' => 12,
-      'B' => 11,
-      'B-' => 10,
-      'C+' => 9,
-      'C' => 8,
-      'C-' => 7,
-      'D+' => 6,
-      'D' => 5,
-      'D-' => 4,
-      'F' => 1
+      'A' => 13,
+      'A-' => 12,
+      'B+' => 11,
+      'B' => 10,
+      'B-' => 9,
+      'C+' => 8,
+      'C' => 7,
+      'C-' => 6,
+      'D+' => 5,
+      'D' => 4,
+      'D-' => 3,
+      'F' => 0
     }
     class << self
       def call(grade: grade)
