eecc1f75136425a0fdcf879fb4c9cf4345b27e69
Frank Showalter <fshowalter@gmail.com>
2016-04-19 15:05:35 -0400

    tweak fonts / remove fast click

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,5 +1,4 @@
 // = require _load-backdrops
-// = require _fastclick
 // = require _toggle-search
 // = require _carnac
 
@@ -10,12 +9,6 @@
     var fastClick;
 
     window.Carnac.on('change', function handleCarnacChange(isInitial) {
-      if (fastClick) {
-        fastClick.destroy();
-      }
-
-      fastClick = window.FastClick.attach(document.body);
-
       if (!isInitial) {
         window.MovielogBackdrops.update();
       }
