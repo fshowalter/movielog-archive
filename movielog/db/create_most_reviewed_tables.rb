ed61f2ce243eb871d3166f6907662343a8194954
Frank Showalter <fshowalter@gmail.com>
2016-05-03 05:35:25 -0400

    rework related

--- movielog/db/create_most_reviewed_tables.rb
+++ movielog/db/create_most_reviewed_tables.rb
@@ -22,24 +22,24 @@ module Movielog
 
         private
 
-        def reviews_table_schema
-          <<-SQL
-          DROP TABLE IF EXISTS "reviews";
-          CREATE TABLE "reviews" (
-            "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            "title" varchar(255) NOT NULL,
-            "date" date NOT NULL,
-            CONSTRAINT fk_reviews_title FOREIGN KEY ("title") REFERENCES "movies" ("title"));
-          CREATE INDEX "index_reviews_on_title" ON "reviews" ("title");
-          SQL
-        end
-
         # rubocop:disable Metrics/LineLength
         # rubocop:disable Metrics/MethodLength
         def most_reviewed_schema(type:, table:, threshold:)
           <<-SQL
-          DROP VIEW IF EXISTS most_reviewed_#{type};
-          CREATE VIEW most_reviewed_#{type} AS
+          DROP TABLE IF EXISTS most_reviewed_#{type};
+          CREATE TABLE "most_reviewed_#{type}" (
+            "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            "full_name" varchar(255) NOT NULL,
+            "movie_count" INTEGER NOT NULL,
+            "reviewed_movie_count" INTEGER NOT NULL,
+            "percent_reviewed" FLOAT NOT NULL,
+            "review_count" INTEGER NOT NULL,
+            "most_recent_review_date" DATE NOT NULL);
+
+          CREATE INDEX "index_most_reviewed_#{type}_on_review_count" ON "most_reviewed_#{type}" ("review_count");
+
+          INSERT INTO most_reviewed_#{type}(
+            full_name, movie_count, reviewed_movie_count, percent_reviewed, review_count, most_recent_review_date)
           SELECT c.full_name,
           COUNT(DISTINCT c.title) AS movie_count,
           COUNT(DISTINCT r.title) AS reviewed_movie_count,
@@ -52,6 +52,8 @@ module Movielog
           HAVING review_count >= #{threshold};
           SQL
         end
+        # rubocop:enable Metrics/LineLength
+        # rubocop:enable Metrics/MethodLength
       end
     end
   end
