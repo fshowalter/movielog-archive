ed1f74e4e2b479b5b9092c7908b3f2a2dee03755
Frank Showalter <fshowalter@gmail.com>
2016-08-07 15:51:03 -0400

    more work

---
+++ movielog/console/update_reviews.rb
@@ -0,0 +1,19 @@
+require 'inquirer'
+
+module Movielog
+  module Console
+    #
+    # Responsible for providing a console interface to update reviews.
+    #
+    class UpdateReviews
+      class << self
+        #
+        # Responsible for processing an update reviews command.
+        #
+        def call
+          Movielog::Db::UpdateMostReviewedTables.call(db: Movielog.db, reviews: Movielog.reviews)
+        end
+      end
+    end
+  end
+end
---
+++ movielog/console/update_viewings.rb
@@ -0,0 +1,19 @@
+require 'inquirer'
+
+module Movielog
+  module Console
+    #
+    # Responsible for providing a console interface to update viewings.
+    #
+    class UpdateViewings
+      class << self
+        #
+        # Responsible for processing an update viewings command.
+        #
+        def call
+          Movielog::Db::UpdateMostWatchedTables.call(db: Movielog.db, viewings: Movielog.viewings)
+        end
+      end
+    end
+  end
+end
