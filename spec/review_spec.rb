fa093c14ec29f07a3ed38d5632fe6fb3d54b2ea3
Frank Showalter <fshowalter@gmail.com>
2016-08-14 16:01:37 -0400

    add specs

--- spec/review_spec.rb
+++ spec/review_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Movielog::Review do
   describe '#title_without_year' do
     it 'returns the title without the year' do
-      review = Movielog::Review.new({ title: 'Fright Night (1985)' })
+      review = Movielog::Review.new(title: 'Fright Night (1985)')
       expect(review.title_without_year).to eq 'Fright Night'
     end
 
     describe 'when year has roman numeral annotation' do
       it 'returns the title without the year' do
-        review = Movielog::Review.new({ title: 'She (1965/I)' })
+        review = Movielog::Review.new(title: 'She (1965/I)')
         expect(review.title_without_year).to eq 'She'
       end
     end
@@ -17,13 +17,13 @@ describe Movielog::Review do
 
   describe '#year' do
     it 'returns the year' do
-      review = Movielog::Review.new({ db_title: 'Fright Night (1985)' })
+      review = Movielog::Review.new(db_title: 'Fright Night (1985)')
       expect(review.year).to eq '1985'
     end
 
     describe 'when year has roman numeral annotation' do
       it 'returns the year without annotation' do
-        review = Movielog::Review.new({ db_title: 'She (1965/I)' })
+        review = Movielog::Review.new(db_title: 'She (1965/I)')
         expect(review.year).to eq '1965'
       end
     end
