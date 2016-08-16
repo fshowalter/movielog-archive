05ca6e1e06a0efe1dbfca5d1cff5f9096373f543
Frank Showalter <fshowalter@gmail.com>
2016-08-15 21:14:13 -0400

    add spec

---
+++ spec/helpers/select_filter_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'support/stub_template_context'
+
+describe Movielog::Helpers do
+  let(:context) { stub_template_context }
+  describe '#select_filter' do
+    it 'returns a javascript select filter' do
+      filter = context.select_filter(
+          label: 'Venue',
+          attribute: 'venue',
+          options: [['venue 1', 0], ['venue 2', 1]])
+
+      expect(filter).to eq(
+        "<div class=\"filter-control\">" \
+        "<label for=\"venue\" class=\"filter-label\">Venue</label>" \
+        "<select name=\"venue\" class=\"filter-select\" " \
+        "data-filter-attribute=\"venue\" data-filter-type=\"select\">" \
+        "<option value=\"\">All</option>\n" \
+        "<option value=\"0\">venue 1</option>\n" \
+        "<option value=\"1\">venue 2</option>\n</select></div>")
+    end
+  end
+end
