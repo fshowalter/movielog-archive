d3ed061ae2b62864f9e93ff90882b705e03acb22
Frank Showalter <fshowalter@gmail.com>
2016-08-28 21:47:18 -0400

    fix specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,12 +8,12 @@ Bundler.require
 
 require 'rspec'
 
-require_relative '../movielog/movielog'
-require 'movie_db/rspec'
-
 RSpec.configure do |config|
   config.order = 'random'
 end
+
+require_relative '../movielog/movielog'
+require 'movie_db/rspec'
 
 require 'fakeweb'
 FakeWeb.allow_net_connect = false
