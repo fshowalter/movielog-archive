5a76a60952131055fd66fc836cb1d75ece8e2cd5
Frank Showalter <fshowalter@gmail.com>
2017-01-02 22:52:24 -0500

    fix viewing title filter

--- movielog/helpers/data_for_viewing.rb
+++ movielog/helpers/data_for_viewing.rb
@@ -9,7 +9,7 @@ module Movielog
 
       {
         data: {
-          title: movie.title,
+          title: movie.display_title,
           sort_title: movie.sortable_title,
           release_date: movie.release_date.iso8601,
           release_date_year: movie.release_date.year,
